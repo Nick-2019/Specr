7b9a6917a0ed1bc9d23f00977804036c534c6e05
Nick-2013 <nick-2013@live.com>
2019-06-28 15:27:16 -0500

    Frontend start

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Containers/Homepage'
+import ComputerContainer from './Containers/ComputerContainer'
+import ComputerPage from './Containers/ComputerPage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Homepage />
+        <ComputerPage />
+        <ComputerContainer />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
-        <a
+        <h1
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Bird up! The WORST show on television!
+        </h1>
       </header>
     </div>
   );
---
+++ frontend/src/Containers/ComputerCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+
+export default class ComputerCard extends Component {
+
+render(){
+    return(
+        <div>
+            <h2>
+            Computer card
+            </h2>
+        </div>
+    )
+}
+}
