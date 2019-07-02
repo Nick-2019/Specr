8c0b6033cbf55dd4a3b76f3e20be098296608a3c
Nick-2013 <nick-2013@live.com>
2019-07-02 14:12:49 -0500

    Filtering 60%

--- frontend/src/Containers/Survey.js
+++ frontend/src/Containers/Survey.js
@@ -14,12 +14,76 @@ export default class Survey extends Component{
 
     surveyTake = (e) => {
         e.preventDefault()
+        let arr = []
+        let arr1 = []
+        let arr2 = []
+        let start = this.state.computers
+        // First question- Primary Use
         let i = e.target.parentElement[0].value
-        let obj = JSON.parse(i)
+        let firstObj = JSON.parse(i)
+        // debugger
 
+
+        //Second Question- Gaming
+        let j = e.target.parentElement[1].value
+        let secondObj = JSON.parse(j)
         debugger
+
+        //Third Question- School
+        let k = e.target.parentElement[2].value
+        let thirdObj = JSON.parse(k)
+
+        //Fourth Question- Streaming 
+        let l = e.target.parentElement[3].value
+        let fourthObj = JSON.parse(l)
+
+        //Fith Question- Buisness
+        let m = e.target.parentElement[4].value
+        let fithObj = JSON.parse(m)
+        //Time to start the filter nonsense
+        //Filtering on the first question
+        start.filter(computer => {
+            //Part 1
+            computer.cpuTier >= firstObj.cpu ? arr.push(computer) : console.log("no")
+            // debugger
+        })
+        console.log(arr)
+        arr.filter(computer => {
+            // Part 2
+            computer.gpuTier >= firstObj.gpu ? arr1.push(computer) : console.log("no")
+        })
+        console.log(arr1)
+
+        //GPU filtering on Question 2
+        secondObj === null ? arr1.map(computer => {arr2.push(computer)}) : arr1.filter(computer => {
+            computer.gpuTier >= secondObj.gpu ? arr2.push(computer) : console.log("no")
+        })
+
+        //OS suggestion on Question 1
+        // let osSuggest = firstObj.os 
+        debugger
+        // if(firstObj.os !== undefined){
+        //     // debugger
+        //     osSuggest = firstObj.os
+        //     debugger
+        // }
+
+
+        console.log(arr2)
+        // debugger
+
+
+
+
+
+
+
+
+
+        // debugger
         this.setState({
-            tookSurvey: true
+            tookSurvey: true,
+            display: arr2
         })
     }
 
@@ -29,7 +93,10 @@ export default class Survey extends Component{
             status: "gaming",
             computers: [],
             display: [],
-            tookSurvey: false
+            tookSurvey: false,
+            recomendcpu: null,
+            recomendos: null
+
             // isLoading: true
         }
     }
--- frontend/src/Containers/SurveyAssets/SComputerContainer.js
+++ frontend/src/Containers/SurveyAssets/SComputerContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import CompCard from './SComputerCard'
 
 export default class Container extends Component{
     render(){
         return(
             <div>
                 This is a container lol
+                {this.props.comps.map(comp => <CompCard comp={comp} /> )}
             </div>
         )
     }
